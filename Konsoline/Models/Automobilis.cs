a2122f4ed149ebdf32e4d02e82986e770be8b31d
Sarunas <sarugen12@gmail.com>
2024-07-23 08:38:09 +0200

    OOP intro

---
+++ Konsoline/Models/Automobilis.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konsoline.Models
+{
+    public class Automobilis
+    {
+        public string Marke { get; set; }
+        public string Modelis { get; set; }
+        public DateOnly PagaminimoMetai { get; set; }
+        public int Rida { get; set; }
+        private string Spalva { get; set; } = "Juoda";
+        public Automobilis()
+        {
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="marke">Automobilio Marke</param>
+        /// <param name="modelis">Automobilio Modelis</param>
+        /// <param name="pagaminimoMetai">Pagaminimo metai (tik data, DateOnly)</param>
+        /// <param name="rida">Rida (sveikasis skaicius)</param>
+        public Automobilis(string marke, string modelis, DateOnly pagaminimoMetai, int rida)
+        {
+            Marke = marke;
+            Modelis = modelis;
+            PagaminimoMetai = pagaminimoMetai;
+            Rida = rida;
+            AtsuktiRida();
+        }
+        public Automobilis(string marke, string modelis, DateOnly pagaminimoMetai, int rida, string spalva)
+        {
+            Marke = marke;
+            Modelis = modelis;
+            PagaminimoMetai = pagaminimoMetai;
+            Rida = rida;
+            Spalva = spalva;
+            AtsuktiRida();
+        }
+        private void AtsuktiRida()
+        {
+            Random r = new Random();
+            Rida -= r.Next(100000, 200000);
+        }
+        public void Perdazyti(string naujaSpalva)
+        {
+            Spalva = naujaSpalva;
+        }
+        public string GautiSpalva()
+        {
+            return Spalva;
+        }
+        public override string ToString()
+        {
+            return $"{Marke} {Modelis} {PagaminimoMetai} Automobilio rida: {Rida}km Automobilio Spalva: {Spalva}";
+        }
+    }
+}
--- Konsoline/Program.cs
+++ Konsoline/Program.cs
@@ -1,79 +1,30 @@
-﻿using System;
-using ManoPrograma.Models;
-using MatematiniaiVeiksmai;
+﻿using Konsoline.Models;
+using System;
+
+
 
 namespace ManoPrograma
 {
     public class PagrindineKlase
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            /*Console.WriteLine("Iveskite skaiciu: ");
-            if (ArLyginis(int.Parse(Console.ReadLine())))
+            Automobilis auto1 = new Automobilis("BMW", "330i", DateOnly.Parse("2012-04-12"), 400000);
+            Automobilis auto2 = new Automobilis("Audi", "RS6", DateOnly.Parse("2008-08-19"), 500000, "Raudona");
+
+            Automobilis[] automobiliai = new Automobilis[2];
+
+            automobiliai[0] = auto1;
+            automobiliai[1] = auto2;
+
+            automobiliai[0].Perdazyti("Melyna");
+           
+            for(int i = 0; i< automobiliai.Length; i++)
             {
-                Console.WriteLine("Lyginis");
+                Console.WriteLine(automobiliai[i]);
             }
-            else
-            {
-                Console.WriteLine("Nelyginis");
-            }
-            Console.WriteLine("Iveskite Kaina: ");
-            double kainaBePvm = double.Parse(Console.ReadLine());
-            Console.WriteLine("Kaina su PVM bus: " + SkaiciuotiKainaSuPVM(kainaBePvm));
-            
-            SpausdinkZvaigzdutes();
-            SpausdinkZvaigzdutes();
-            SpausdinkZvaigzdutes();
-            SpausdinkZvaigzdutes();
-            SpausdinkZvaigzdutes();
-            
-            Console.WriteLine("Iveskite ka norite atspausdinti 10 kartu: "); 
-            SpausdinkDesimtSimboliu(Console.ReadLine());
-            
-            Matematika matFunkcijos = new Matematika();
-            Console.WriteLine(matFunkcijos.Sudek(10, 12));*/
-            Zmogus.StatineVerte = 94;
-            Zmogus x = SugeneruokZmogausParametrus("Jonas", "Jonaitis");
-            Zmogus y = SugeneruokZmogausParametrus("Petras", "Petraitis");
-            Zmogus z = SugeneruokZmogausParametrus("Vaidas", "Vaidaitis");
-            y.PadidinkUgi(35);
-            Zmogus.StatineVerte = 85;
-            Console.WriteLine(z.GimimoMetus);
         }
 
-        public static bool ArLyginis(int skaicius)
-        {
-            if (skaicius % 2 == 0)
-                return true;
-            return false;
-        }
-
-        public static double SkaiciuotiKainaSuPVM(double grossPrice)
-        {
-            return grossPrice * 1.21;
-        }
-
-        public static void SpausdinkZvaigzdutes()
-        {
-            Console.WriteLine("************************************");
-        }
-        public static void SpausdinkDesimtSimboliu(string s)
-        {
-            Console.WriteLine($"{s}{s}{s}{s}{s}{s}{s}{s}{s}");
-        }
-
-        public static Zmogus SugeneruokZmogausParametrus(string vardas, string pavarde)
-        {
-            Random r = new Random();
-            
-            Zmogus zmogus = new Zmogus();
-            zmogus.Vardas = vardas;
-            zmogus.Pavarde = pavarde;
-            zmogus.GimimoMetus = r.Next(1950, 2020);
-            zmogus.Ugis = r.Next(100, 220);
-
-            return zmogus;
-        }
     }
 }
 
